0d674482fbd9e65e743a3e0622ee5bd9976f99bb
GreenAvocado92 <496098804@qq.com>
2024-03-19 09:37:51 +0800

    first commit

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/FloatMath.cpp.o"
+  "CMakeFiles/main.dir/src/InParser.cpp.o"
+  "CMakeFiles/main.dir/src/SaveUSDA.cpp.o"
+  "CMakeFiles/main.dir/src/main.cpp.o"
+  "CMakeFiles/main.dir/src/wavefront.cpp.o"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
